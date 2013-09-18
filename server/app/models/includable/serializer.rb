f48da7950226a0ba5e8e525e186da07e28fd3bf5
Felix Tioh <felixsagitta@gmail.com>
2013-09-18 15:16:32 +0800

    try fixing

--- server/app/models/includable/serializer.rb
+++ server/app/models/includable/serializer.rb
@@ -5,14 +5,8 @@ module Includable
       serializer = self.class.name + 'Serializer'
       p serializer
       if serializer.safe_constantize
-        p "===================="
-        p "YES!!!!!!!!!!!!!"
-        p "===================="
         serializer.constantize.new(self).as_json(options)
       else
-        p "===================="
-        p "NONONONO"
-        p "===================="
         super(options)
       end
     end
