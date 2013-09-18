fce41f711f1fa3dd47664366f27ebabee1c23726
Felix Tioh <felixsagitta@gmail.com>
2013-09-18 23:36:05 +0800

    can push now

--- server/app/mailers/project_mailer.rb
+++ server/app/mailers/project_mailer.rb
@@ -2,8 +2,8 @@ class ProjectMailer < ActionMailer::Base
   default from: "contactus@drawingboard.com"
 
   AdminEmail = 'felixsagitta@gmail.com'
-  #SiteUrl = 'http://162.243.15.77'
-  SiteUrl = 'http://localhost:3333'
+  SiteUrl = 'http://162.243.15.77'
+  #SiteUrl = 'http://localhost:3333'
 
   def send_request_for_quote(event, provider)
     @event = event
